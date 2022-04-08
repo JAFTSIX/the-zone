c8909240df680dac413c2607776e39eebeb30dbb
JAFTSIX <42688559+JAFTSIX@users.noreply.github.com>
2022-04-08 16:50:58 -0600

    search half done
    it already tell the locations of the dependencies

--- THEZONE/Search.py
+++ THEZONE/Search.py
@@ -1,2 +1,24 @@
+from ast import For
+from configparser import DuplicateSectionError
+import io
+import re
+
+#0 means that the file needs to be on the system
 def search():
-    print('uwu')
+    print('starting the search for MDS dependencies')
+    searchReplaceFile=io.open('.\\search&replace.txt','r')
+    lines=searchReplaceFile.readlines()
+    searchReplaceFile.close()
+    mdslocations={}
+    for line in lines:
+        if line.rfind('<oramds:/apps>/AIAMetaData/AIAComponents')>-1:
+            #copy everithing inside""
+            line=re.findall(f'".*?"',line)[0]
+            result=re.sub('"<oramds:/apps>','',line)[:-1]
+            mdslocations[result]=0
+
+    print(mdslocations)
+
+
+
+search()    
